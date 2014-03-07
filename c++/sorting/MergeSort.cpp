4397d991871b8f099e2ed732b91d317956b839c8
alex <adalyac@gmail.com>
2014-03-07 15:59:45 +0000

    working on merge

--- c++/sorting/MergeSort.cpp
+++ c++/sorting/MergeSort.cpp
@@ -11,7 +11,12 @@ void MergeSort::sort(std::vector<int> &A)
 /******************************************************/
 void MergeSort::sortpart(std::vector<int> &A, int p, int r)
 {
-	/*********** INSERT YOUR CODE HERE ****************/
+  if (p < r) {
+    int q = (p+r)/2;
+    sortpart(A, p, q);
+    sortpart(A, q+1, r);
+    merge(A, p, q, r);
+  }
 }
 
 /******************************************************/
@@ -19,5 +24,22 @@ void MergeSort::sortpart(std::vector<int> &A, int p, int r)
 /******************************************************/
 void MergeSort::merge(std::vector<int> &A, int p, int q, int r)
 {
-	/*********** INSERT YOUR CODE HERE ****************/
+  //A[p:q] is sorted, A[q+1:r] is sorted, latter array might have size one greater
+  int temp[2];
+  for (int i=0; i<=q-p; i++) { //i<q-p+1?
+    if (A[p+i] <= A[q+1+i]) {
+      temp[0] = A[p+i+1];   //ok but when will temp[0] be reinserted?
+      A[p+i+1] = A[q+1+i];
+    }
+    else {
+      temp[0] = A[p+i];
+      A[p+i] = A[q+1+i];
+      temp[1] = A[p+i+1];   //ok but when will temp[1] be reinserted?
+      A[p+i+1] = temp[0];
+    }
+  }
+
+  if ((p+r) % 2 == 1) { //if A[q+1:r] has size 1 more than A[p:q]
+
+  }
 }
