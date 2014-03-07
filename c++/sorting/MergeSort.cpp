cd9519a85fcdb61d3cf86c64ba96a9482516c439
alex <adalyac@gmail.com>
2014-03-07 19:17:25 +0000

    new version of merge, about to try

--- c++/sorting/MergeSort.cpp
+++ c++/sorting/MergeSort.cpp
@@ -32,35 +32,43 @@ void MergeSort::merge(std::vector<int> &A, int p, int q, int r)
   //A[p:q] is sorted, A[q+1:r] is sorted, A[p:q] has size one greater
   std::vector<int> copyOfA(A);
 
-  
-  for (int i=0; i<=r-q-1; i++) { 
-    if (copyOfA[p+i] <= copyOfA[q+1+i]) {
-      A[p+(2*i)] = copyOfA[p+i];
-      A[p+(2*i)+1] = copyOfA[q+1+i];  //A[p+i] in its right place
+  int n=p, a=p, b=q+1;
+  while (a<=q && b<=r) {
+    if (copyOfA[a] <= copyOfA[b]) {
+      A[n] = copyOfA[a];
+      a++, n++;
+      std::cout << "sorted " << copyOfA[a-1] << ". array now:" << std::endl;
+      for(int k = p; k<=r; k++)
+	std::cout << A[k] << ' ';
+      std::cout << std::endl;
     }
     else {
-      A[p+(2*i)] = copyOfA[q+1+i];
-      A[p+(2*i)+1] = copyOfA[p+i];
+      A[n] = copyOfA[b];
+      b++, n++;
+      std::cout << "sorted " << copyOfA[b-1] << ". array now:" << std::endl;
+      for(int k = p; k<=r; k++)
+	std::cout << A[k] << ' ';
+      std::cout << std::endl;
     }
-
-    
-    std::cout << "sorted " << copyOfA[p+i] << ", " << copyOfA[q+1+i] << ". array now:" << std::endl;
-    for(int k = p; k<=r; k++)
-      std::cout << A[k] << ' ';
-    std::cout << std::endl;
   }
-
   
-  if ((p+r) % 2 == 0) { //if A[p:q] has size 1 more than A[q+1:r]
-    if (A[r] > copyOfA[q])
-      A[r-1] = copyOfA[q];
-    else A[r] = copyOfA[q];
-
-    
-    std::cout << "sorted " << copyOfA[q] << ". array now:" << std::endl;
-    for(int k = p; k<=r; k++)
-      std::cout << A[k] << ' ';
-    std::cout << std::endl;
+  while (a!=q) {
+    A[n] = copyOfA[a];
+    a++, n++;
+      std::cout << "sorted " << copyOfA[a-1] << ". array now:" << std::endl;
+      for(int k = p; k<=r; k++)
+	std::cout << A[k] << ' ';
+      std::cout << std::endl;
   }
-  std::cout << std::endl;
+  
+  while (b!=r) {
+    A[n] = copyOfA[b];
+    b++, n++;
+      std::cout << "sorted " << copyOfA[b-1] << ". array now:" << std::endl;
+      for(int k = p; k<=r; k++)
+	std::cout << A[k] << ' ';
+      std::cout << std::endl;
+  }
+
+  assert(n==r);
 }
