a850a38d6e477d480b16132ecb8036a2e0ef96e5
alex <adalyac@gmail.com>
2014-03-11 17:38:54 +0000

    working on hybrid

--- c++/sorting/HybridSort.cpp
+++ c++/sorting/HybridSort.cpp
@@ -1,6 +1,7 @@
 #include "HybridSort.hpp"
 #include "InsertionSort.hpp"
 #include "MergeSort.hpp"
+#include <iostream>
 
 void HybridSort::sort(std::vector<int> &A)
 {
@@ -11,9 +12,22 @@ void HybridSort::sort(std::vector<int> &A)
 /*************** COMPLETE THIS FUNCTION ***************/
 /******************************************************/
 void HybridSort::sortpart(std::vector<int> &A, int p, int r)
-{
-  if (p - r < 8) {
-    InsertionSort::sortpart(A, p, r);
+{ 
+  if (r - p <= 8) {
+    // std::cout << "using insertion to sort ";
+    // for(int k = p; k<=r; k++)
+    //   std::cout << A[k] << ' ';
+    // std::cout << std::endl;
+
+    for (int j=p+1; j<=r; j++) {
+      int key = A[j];
+      int i = j-1;
+      while (i>=0 && A[i] > key) {
+	A[i+1] = A[i];
+	i--;
+      }
+      A[i+1] = key;
+    }
   }
   else {
     int q = (p+r)/2;
