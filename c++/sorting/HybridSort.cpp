3d18e0b56a5ce46d11f93c19f704e83ed902f231
alex <adalyac@gmail.com>
2014-03-11 18:08:04 +0000

    think this is optimal hybridsort (apart from vector copy rather than array)

--- c++/sorting/HybridSort.cpp
+++ c++/sorting/HybridSort.cpp
@@ -13,22 +13,8 @@ void HybridSort::sort(std::vector<int> &A)
 /******************************************************/
 void HybridSort::sortpart(std::vector<int> &A, int p, int r)
 { 
-  if (r - p <= 8) {
-    // std::cout << "using insertion to sort ";
-    // for(int k = p; k<=r; k++)
-    //   std::cout << A[k] << ' ';
-    // std::cout << std::endl;
-
-    for (int j=p+1; j<=r; j++) {
-      int key = A[j];
-      int i = j-1;
-      while (i>=0 && A[i] > key) {
-	A[i+1] = A[i];
-	i--;
-      }
-      A[i+1] = key;
-    }
-  }
+  if (r - p <= 4)
+    InsertionSort::sortpart(A, p, r);
   else {
     int q = (p+r)/2;
     HybridSort::sortpart(A, p, q);   //HybridSort:: ?
